ac757eebf8c456c02fa1c6a67b426b57870df79b
vivekjami <j.vivekvamsi@gmail.com>
2025-05-21 22:29:45 +0530

    ok1

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -6,7 +6,7 @@ import { DndContext, closestCenter, KeyboardSensor, PointerSensor, useSensor, us
 import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
 import TaskItem from './TaskItem';
-import { motion, AnimatePresence } from 'framer-motion';
+// import {  AnimatePresence } from 'framer-motion';
 
 export default function TaskList() {
   const { tasks, reorderTasks, isLoading } = useTasks();
